02d1bc1e0855473e69ecb80502e3fbbe5bc83be2
Jakob Relling <jakobere@stud.ntnu.no>
2022-03-03 21:08:48 +0100

    Endre på funksjonalitet, tile-klasser

--- src/main/java/game2048/Grid.java
+++ src/main/java/game2048/Grid.java
@@ -6,51 +6,69 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Grid {
-    ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
+    ArrayList<ArrayList<Tile>> rows = new ArrayList<>();
 
     public Grid() {
         makeGrid();
-        System.out.println(this.rows);
-        randomNewNumber();
-        System.out.println(this.rows);
+        System.out.println(this);
+        //randomNewNumber();
         moveRight();
-        System.out.println(this.rows);
+        System.out.println(this);
+        moveLeft();
+        System.out.println(this);
+       
 
     }
 
     public void makeGrid() {
         for (int x=0; x < 4; x++) {
-            ArrayList<Integer> row = new ArrayList<Integer>();
+            ArrayList<Tile> row = new ArrayList<Tile>();
             for (int y=0; y<4; y++) {
-                row.add(0);
+                if (y == 2) row.add(new Tile(x,y,1));
+                else {
+                    row.add(new Tile(x,y,0));
+                }
             }
             this.rows.add(row);
         }
     }
 
     public void moveRight() {
-        ArrayList<ArrayList<Integer>> rader = this.rows;
-        ArrayList<Integer> tomListe = new ArrayList<>(Arrays.asList(0,0,0,0));
+        ArrayList<ArrayList<Tile>> rader = this.rows;
+        ArrayList<Tile> tomListe = new ArrayList<>();
         for (int x=0; x < 4; x++) {
-            ArrayList<Integer> rad = horizontalMove(rader.get(x));
-            rader.set(x,tomListe);
+            ArrayList<Tile> rad = horizontalMove(rader.get(x),0);
+            rader.set(x, tomListe);
             rader.set(x, rad);
         }
     }
 
-    public ArrayList<Integer> horizontalMove(ArrayList<Integer> rad) {
+    public ArrayList<Tile> horizontalMove(ArrayList<Tile> rad, int z) {
+        if (z == 1) Collections.reverse(rad);
         for (int x=0; x < 4; x++) {
             if (x < 3) {
-                if (rad.get(x) != 0 && rad.get(x+1).equals(0)) {
+                if (rad.get(x).getValue() != 0 && (rad.get(x+1).getValue()).equals(0)) {
                     Collections.swap(rad,x,x+1);
                 }
             }
         }
+        if (z==1) Collections.reverse(rad);
         return rad;
-        
     }
 
-    public ArrayList<ArrayList<Integer>> randomNewNumber() {
+    public void moveLeft() {
+        ArrayList<ArrayList<Tile>> rader = this.rows;
+        ArrayList<Tile> tomListe = new ArrayList<>();
+        for (int x=0; x < 4; x++) {
+            ArrayList<Tile> rad = horizontalMove(rader.get(x),1);
+            rader.set(x, tomListe);
+            rader.set(x, rad);
+        }
+        
+    }
+    
+
+    /* public ArrayList<ArrayList<Integer>> randomNewNumber() {
         ArrayList<String> emptySquare = new ArrayList<>();
         for (int x=0; x < 4; x++) {
             for (int y=0; y<4; y++) {
@@ -80,6 +98,19 @@ public class Grid {
             this.rows.get(x).set(y,i);
         }
         return this.rows;
+    } */
+
+    @Override
+    public String toString() {
+        String utskrift = ""; 
+        for (int x=0; x < 4; x++) {
+            for (int y=0; y < 4; y++) {
+                utskrift += "[";
+                utskrift += "["+this.rows.get(x).get(y).getValue()+"]";
+            }
+            utskrift += "]\n";
+        }
+        return utskrift;
     }
 
     public static void main(String[] args) {
--- src/main/java/game2048/Tile.java
+++ src/main/java/game2048/Tile.java
@@ -1,5 +1,28 @@
 package game2048;
 
 public class Tile {
-    
+    private int x;
+    private int y;
+    private Integer value;
+
+    public Tile(int x, int y, Integer value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return this.value; 
+    }
+
+    public void mergeWith(Tile newTile) {
+        this.value = this.value + newTile.value;
+    }
+
+    public boolean canMerge(Tile newTile) {
+        return this.value == newTile.value;
+    }
+
+
+
 }
